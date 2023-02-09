6424fd7f971dfd96082f1e55b48a040951d53086
ConnerKT <connerthompson121@gmail.com>
2023-02-08 23:05:45 -0600

    JavaScript
    Added JavaScript functionality, added its own file folder for external scripting.

---
+++ script.js
@@ -0,0 +1,16 @@
+let a =1;
+function lesson(){
+    let b = 2
+    console.log(a);
+    console.log(b);   
+}  
+
+console.log(b);
+
+
+
+function whodat() {
+   
+    let name = prompt("What is your name?: ");
+    console.log("Hello, " + name);
+}
