96380be8bb383ce02ac4f872cffafd1acc316c41
ConnerKT <connerthompson121@gmail.com>
2023-02-15 00:34:01 -0600

    JS Addition
    Here's what I added!
    - Two Buttons (One button will change the background, the other will introduce!)
    -Included confirm component
    -Included prompts
    -Used if statements
    -Added fun easter eggs

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-function random(1, 100){
-    return Math.random() * (1 - 100) + min;
+let colors = ["#F1FFC4","#C6E2E9","#A7BED3","#FFCAAF","#1a1a1a"]
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+    var randomColor = colors[Math.floor(Math.random() * colors.length)]
+    let body = document.getElementById('body');
+
+    body.style.background = randomColor
+})
+let buttonclicks = 0
+function buttonclick(){
+    if (buttonclicks < 2){
+        confirm("Are you sure you want to change the background? It may lead to weird side effects.....")
+    }else if (buttonclicks === 2){
+        confirm("Hey are you sure...?")
+    }else if (buttonclicks === 4){
+        confirm("Don't your eyes hurt...?")
+
 }
+buttonclicks++
+}
+
+
+var username;
+ function display(){
+    var username = prompt("Your name:", "");
+    if (username == "") {
+        alert("Name must be inputed!")
+        display();
+ }else
+         alert ("Hello " + username );
+   
+ }
